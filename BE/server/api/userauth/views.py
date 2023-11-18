6467547cc0e97a50811885b844ccffac7886172c
splix <federicosalalois@gmail.com>
2023-11-18 11:35:48 +0100

    based of mateo-fede

--- BE/server/api/userauth/views.py
+++ BE/server/api/userauth/views.py
@@ -1,44 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from .models import CustomUser
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 import json
-from passlib.hash import pbkdf2_sha256
-from django.middleware.csrf import get_token
 
-def sendToken_view(request):
-	csrf_token = get_token(request)
-	return JsonResponse({'token': csrf_token})
-
-def get_user_from_username(username):
-	try:
-		user = CustomUser.objects.get(username=username)
-		return user
-	except CustomUser.DoesNotExist:
-		return None
-
-@csrf_exempt
-def get_user_by_username(request):
-	if request.method == 'GET':
-		try:
-			data = json.loads(request.body.decode('utf-8'))
-			username = data.get('username')
-			if username:
-				user = get_user_from_username(username)
-				if user:
-					return JsonResponse({'user_id': user.id, 'username': user.username})
-				else:
-					return JsonResponse({'message': 'User not found'}, status=404)
-			else:
-				return JsonResponse({'message': 'Username missing in the request body'}, status=400)
-
-		except json.JSONDecodeError:
-			return JsonResponse({'message': 'Invalid JSON in the request body'}, status=400)
-
-	return JsonResponse({'message': 'Only GET requests are allowed'}, status=400)
-
+# Create your views here.
 def signup(request):
     if request.method == 'POST':
         try:
@@ -70,10 +36,6 @@ def signup(request):
 
     return JsonResponse({'status': 'error', 'message': 'Only POST requests are allowed'}, status=400)
 
-
-def signupIntra(request):
-	pass
-
 def login(request):
     if request.method == 'POST':
         try:
@@ -100,19 +62,3 @@ def login(request):
             return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
 
     return JsonResponse({'status': 'error', 'message': 'Only POST requests are allowed'}, status=400)
-
-@csrf_exempt
-def get_user_from_username_view(request, username):
-	try:
-		user = CustomUser.objects.get(username=username)
-		user = {
-			'username': user.username,
-			'fullname': user.fullname
-		}
-		return JsonResponse({'user': user})
-	except CustomUser.DoesNotExist:
-		return JsonResponse({'error': 'User not found'}, status=404)
-
-@csrf_exempt
-def testView(request):
-	return JsonResponse({'message': 'Test passed'})
--- BE/server/server/urls.py
+++ BE/server/server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoAPI project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path("user/", include("userauth.urls"))
+    path('api/', include('api.urls')),
+	
 ]
