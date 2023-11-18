c098d3acb5ea2536d3eccf42de845ac8945afa48
splix <federicosalalois@gmail.com>
2023-11-18 12:08:28 +0100

    testing tourney views

--- BE/server/api/tournament/urls.py
+++ BE/server/api/tournament/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, test_match
 
 
 urlpatterns = [
 	path('', test_view, name="test"),
+    path('test/', test_match, name="test")
 ]
--- BE/server/api/tournament/views.py
+++ BE/server/api/tournament/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-
-@csrf_exempt
 def test_view(request):
-	return JsonResponse({'Tournament': 'Not Today'})
+	return JsonResponse({'Tournament': 'Not Today!'})
+
+def test_match(request):
+	match = {
+		'player1': 'Player1',
+		'player2': 'Player2',
+		'winner': 'Player1',
+		'loser': 'Player2',
+		'round': 1
+	}
+	return JsonResponse(match)
