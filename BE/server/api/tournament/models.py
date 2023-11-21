4dfad564add0ab262df02291c2a694f38c2abc3a
splix <federicosalalois@gmail.com>
2023-11-21 19:09:26 +0100

    added tests.py

--- BE/server/api/tournament/models.py
+++ BE/server/api/tournament/models.py
@@ -11,10 +11,11 @@ class Match(models.Model):
     def __str__(self):
         return f"Match {self.id} - {self.player1} vs {self.player2}"
 
+
 class Tournament(models.Model):
     name = models.CharField(max_length=100)
-    start_date = models.DateTimeField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     matches = models.ManyToManyField(Match, blank=True)
 
     def __str__(self):
