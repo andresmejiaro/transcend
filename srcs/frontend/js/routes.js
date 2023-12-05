5dff88ae278d72b9613f021d78a30b0baddc7291
adrgonza <adrian.gonser@icloud.com>
2023-12-05 04:47:55 +0100

    landing page and index improved

--- srcs/frontend/js/routes.js
+++ srcs/frontend/js/routes.js
@@ -1,87 +1,100 @@
 const urlPageTitle = "Pixel Pong";
 
 const urlRoutes = {
+
   404: {
     template: "/templates/404.html",
     title: "404 | " + urlPageTitle,
     description: "Page not found",
   },
+
   "/": {
-    template: "/templates/index.html",
-    title: "Landing | " + urlPageTitle,
+    template: "/templates/landing.html",
+    title: urlPageTitle,
     description: "This is the landing page",
     js: ["./js/index.js"],
     css: ["./css/landing.css"]
   },
+
   "/home": {
     template: "/templates/home.html",
-    title: "Home | " + urlPageTitle,
+    title: urlPageTitle,
     description: "This is the home page",
     js: ["./js/home.js", "./js/auth/intra-handler.js"],
     css: ["./css/home.css"]
   },
+
   "/login": {
     template: "/templates/login.html",
-    title: "Login | " + urlPageTitle,
+    title: urlPageTitle + " - Sing in",
     description: "This is the login page",
     js: ["./js/auth/login-handler.js"],
     css: ["./css/signup.css", "./css/inputs.css"]
   },
+
   "/signup": {
     template: "/templates/signup.html",
-    title: "Signup | " + urlPageTitle,
+    title: urlPageTitle + " - Sing up",
     description: "This is the login page",
     js: ["./js/auth/signup-handler.js"],
     css: ["./css/signup.css", "./css/inputs.css"]
   },
+  
   "/otp": {
     template: "/templates/otp.html",
-    title: "OTP | " + urlPageTitle,
+    title: urlPageTitle,
     description: "This is the 2FA page",
     js: ["./js/auth/otp-handler.js"],
   },
+
   "/callback": {
     template: "/templates/callback.html",
-    title: "callback | " + urlPageTitle,
+    title: urlPageTitle,
     description: "This is the callback page",
     js: ["./js/auth/callback.js"],
   },
+
   "/home-logged": {
     template: "/templates/home-logged.html",
-    title: "Home- | " + urlPageTitle,
+    title: urlPageTitle,
     description: "This is the home page",
     js: ["./js/home-loggedin.js"],
   },
+
   "/user": {
     template: "/templates/user.html",
-    title: "user | " + urlPageTitle,
+    title: urlPageTitle + " - Profile",
     description: "This is the user page",
     js: ["./js/user/user.js"],
   },
+
   "/user-settings": {
     template: "/templates/user-settings.html",
-    title: "user | " + urlPageTitle,
+    title: urlPageTitle + " - Profile",
     description: "This is the user page",
     js: ["./js/user/user-settings.js"],
     css: ["./css/inputs.css"]
   },
+
   "/create-tournaments": {
     template: "/templates/create-tournament.html",
-    title: "create tournament | " + urlPageTitle,
+    title: urlPageTitle + "Tournaments",
     description: "This is the create tournament page",
     js: ["./js/create-tor.js"],
     css: ["./css/tournaments.css"]
   },
+
   "/leaderboards": {
     template: "/templates/leaderboards.html",
-    title: "Leaderboards | " + urlPageTitle,
+    title: urlPageTitle + " - Leaderboard",
     description: "This is the leaderboards tournament page",
     // js: ["./js/create-tor.js"],
     // css: ["./css/tournaments.css"]
   },
+
   "/local-game": {
     template: "/templates/local-game.html",
-    title: "Local Game | " + urlPageTitle,
+    title: urlPageTitle,
     description: "This is the local pong page",
     js: ["./pongJs/environment.js","./pongJs/MovingRectangle.js",
     "./pongJs/Ball.js","./pongJs/Paddle.js","./pongJs/Player.js", 
