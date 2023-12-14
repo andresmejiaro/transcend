878d188aa0ce05beb7a3d387733df8ebc7cd7d90
splix <federicosalalois@gmail.com>
2023-12-15 00:11:12 +0100

    kept the old lobby just in case

--- backend/server/ws_api/routing.py
+++ backend/server/ws_api/routing.py
@@ -1,12 +1,14 @@
 # ws_api/routing.py
 
 from django.urls import re_path
-from .consumers.lobbyconsumer2 import LobbyConsumer
+from .consumers.lobbyconsumer import LobbyConsumer as LobbyConsumer
+from .consumers.lobbyconsumer2 import LobbyConsumer as LobbyConsumer2
 from .consumers.gameconsumerasbride import GameConsumerAsBridge
 from .consumers.tournamentconsumer import TournamentConsumer
 
 websocket_pong_urlpatterns = [
     re_path(r"^ws/lobby/$", LobbyConsumer.as_asgi()),
+    re_path(r"^ws/lobby2/$", LobbyConsumer2.as_asgi()),
     re_path(r"^ws/pong/(?P<match_id>\w+)/$", GameConsumerAsBridge.as_asgi()),
     re_path(r"^ws/tournament/(?P<tournament_id>\w+)/$", TournamentConsumer.as_asgi())
 ]
