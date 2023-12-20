d45190e9a2078cbf9f6cbf6cb5e0937c894f725f
Mateo pizzolo <mateo@Mateos-Mac-mini.local>
2023-12-20 11:29:30 +0100

    Al tratar de conectarse al lobby si el usuario ya existe rechaza la coneccion, desde el front lo deslogueamos

--- frontend/srcs/utils/lobby/lobby-connection-handler.js
+++ frontend/srcs/utils/lobby/lobby-connection-handler.js
@@ -70,6 +70,7 @@ const getOnlineUsers = async () => {
       data: {},
     });
   } catch (error) {
+    handleLogout();
     console.error("Error while connecting to WebSocket:", error.message);
   }
 };
