061e80f6edcf4f919f9938c3cdfb89195070787c
HakimHC <ahmedh90111@gmail.com>
2024-01-04 23:49:35 +0100

    CORS error

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -227,7 +227,9 @@ AUTH_USER_MODEL = 'userauth.CustomUser'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://localhost:5000",
     "http://127.0.0.1:3000",
+    "https://*",
     os.getenv('FRONTEND_URL'),
 ]
 
