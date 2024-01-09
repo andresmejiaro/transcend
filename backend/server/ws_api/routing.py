676cb84fb97b3d39ef6a101914cedc0b442bef80
HakimHC <ahmedh90111@gmail.com>
2024-01-09 10:00:41 +0100

    tournament wip

--- backend/server/ws_api/routing.py
+++ backend/server/ws_api/routing.py
@@ -4,12 +4,13 @@ from django.urls import re_path
 from .consumers.lobbyconsumer import LobbyConsumer as LobbyConsumer
 from .consumers.gameconsumerasbride import GameConsumerAsBridge
 from .consumers.tournamentconsumer import TournamentConsumer
+from .consumers.tournament import Tournament2Consumer
 from .consumers.gameconsumer2 import PongConsumer
 
 websocket_pong_urlpatterns = [
     re_path(r"^ws/lobby/$", LobbyConsumer.as_asgi()),
     re_path(r"^ws/pong/(?P<match_id>\w+)/$", GameConsumerAsBridge.as_asgi()),
-    re_path(r"^ws/tournament/(?P<tournament_id>\w+)/$", TournamentConsumer.as_asgi()),
+    re_path(r"^ws/tournament/(?P<tournament_id>\w+)/$", Tournament2Consumer.as_asgi()),
     re_path(r"^ws/pong2/(?P<match_id>\w+)/$", PongConsumer.as_asgi()),
     
 ]
