b4f60c2fe2893ff2a554debadb2b46091ce884c1
HakimHC <ahmedh90111@gmail.com>
2024-01-16 21:20:23 +0100

    migrating bo3 to new app

---
+++ backend/server/api/best_of_three/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BestOfThreeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'best_of_three'
--- backend/server/api/tournament/models.py
+++ backend/server/api/tournament/models.py
@@ -38,14 +38,3 @@ class Round(models.Model):
 
     def __str__(self):
         return f"Round {self.round_number} of {self.tournament}"
-    
-class BestofThree(models.Model):
-    name = models.CharField(max_length=100)
-    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1')
-    player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player2', null=True)
-    match1 = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='match1', null=True)
-    match2 = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='match2', null=True)
-    match3 = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='match3', null=True)
-    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='winner', blank=True, null=True)
-    date_played = models.DateTimeField(blank=True, null=True)
-    active = models.BooleanField(default=False)
