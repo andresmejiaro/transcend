cf13599c2f18d0a94038a33436778212c1465e9c
HakimHC <ahmedh90111@gmail.com>
2024-01-28 22:04:12 +0100

    User stats based on username READY

--- backend/server/api/userauth/urls.py
+++ backend/server/api/userauth/urls.py
@@ -7,7 +7,7 @@ from .views.user import info_me_view, info_me_id_view, update_avatar_view, get_u
 from .views.token import send_csrf_token_view, validate_jwt_token_view
 from .views.googleauth import enable_2fa, disable_2fa, display_qr_code, verify_totp_code, user_2fa_setup_complete
 from .views.auth import oauth_start, oauth_login
-from .views.stats import get_kpi
+from .views.stats import get_kpi, get_kpi_username
 from .views.user import update_user_information
 
 urlpatterns = [
@@ -41,6 +41,7 @@ urlpatterns = [
     path('oauth/login/', oauth_login, name="oauth-login"),
 
     path('user/stats/', get_kpi, name="get_kpi"),
+    path('user/stats/<str:username>/', get_kpi_username, name="get_kpi_username"),
     path('user/update/', update_user_information, name="update_user_info"),
 
 ]
--- backend/server/api/userauth/views/stats.py
+++ backend/server/api/userauth/views/stats.py
@@ -5,6 +5,7 @@ from api.tournament.models import Match
 from api.tournament.models import Tournament
 from api.jwt_utils import get_user_id_from_jwt_token
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 
 
 def get_kpi(request):
@@ -12,10 +13,6 @@ def get_kpi(request):
         return JsonResponse({
             "status": "Method not allowed"
         }, status=405)
-    # try:
-    #     user = CustomUser.objects.get(pk=user_id)
-    # except CustomUser.DoesNotExist:
-    #     return JsonResponse({"status": "User not found"}, status=404)
 
     authorization_header = request.headers.get('Authorization')
     if authorization_header:
@@ -26,6 +23,36 @@ def get_kpi(request):
             user = CustomUser.objects.get(id=user_id)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=401)
+
+    matches = Match.objects.filter(Q(player1=user) | Q(player2=user))
+    wins = matches.filter(winner=user)
+    winrate = round(len(wins) / len(matches), 2) if len(matches) != 0 else 0
+    tournament_wins = Tournament.objects.filter(winner=user)
+
+    res = {
+        "games_played": len(matches),
+        "wins": len(wins),
+        "winrate": winrate * 100,
+        "tournaments_won": len(tournament_wins),
+    }
+
+    return JsonResponse(res)
+
+
+def get_kpi_username(request, username):
+    if request.method != 'GET':
+        return JsonResponse({
+            "status": "Method not allowed"
+        }, status=405)
+
+    authorization_header = request.headers.get('Authorization')
+    if authorization_header:
+        try:
+            _, token = authorization_header.split()
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=401)
+    
+    user = get_object_or_404(CustomUser, username=username)
 
     matches = Match.objects.filter(Q(player1=user) | Q(player2=user))
     wins = matches.filter(winner=user)
